9a725c765a85cfde429e564b4a257dbd96a0891b
ta.martins <ta.martins@campus.fct.unl.pt>
2020-07-18 23:18:47 +0100

    1st Commit
    Initial Commit

---
+++ Input/Delimiter.py
@@ -0,0 +1,14 @@
+import numpy as np
+import cv2
+from PIL import ImageGrab
+import win32gui
+import time
+import matplotlib.pyplot as plt
+
+
+width = 1000
+height = 700
+
+vertices = []
+
+# Region of interest
