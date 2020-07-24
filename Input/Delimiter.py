f8fbe9759f7387e58d19e3f43e908a3711fd84c1
ta.martins <ta.martins@campus.fct.unl.pt>
2020-07-24 19:35:11 +0100

    Update Delimiter.py

--- Input/Delimiter.py
+++ Input/Delimiter.py
@@ -5,10 +5,46 @@ import win32gui
 import time
 import matplotlib.pyplot as plt
 
-
 width = 1000
 height = 700
 
-vertices = []
+def image_processing(image):
 
-# Region of interest
+    low_level = np.array([18, 94, 140])
+    up_level = np.array([48, 255, 255])
+
+    frameBlur = cv2.GaussianBlur(image, (5, 5), 0)
+    hsv = cv2.cvtColor(frameBlur, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, low_level, up_level)
+    edges = cv2.Canny(mask, 75, 100)
+
+    return edges
+
+while(True):
+
+        # Time Update
+        last_time = time.time()
+
+        screen_recording = ImageGrab.grab(bbox=(0,0,width,height))
+        screenArray = np.array(screen_recording)
+        frameConversion = cv2.cvtColor(screenArray, cv2.COLOR_BGR2RGB)
+
+        edges = image_processing(frameConversion)
+
+        lines = cv2.HoughLinesP(edges, 1, np.pi/180, 50, maxLineGap = 50)
+
+        if lines is not None:
+            for line in lines:
+                x1, y1, x2, y2 = line[0]
+                cv2.line(frameConversion, (x1,y1), (x2,y2), (0,255,0),5)
+
+        cv2.imshow("screen recorder", np.array(frameConversion))
+        cv2.imshow("edges", edges)
+
+        print('Processing Time:', time.time() - last_time)
+        last_time = time.time()
+
+        # Key to stop recording
+        if cv2.waitKey(1) == ord('t'):
+            cv2.destroyAllWindows()
+            break
